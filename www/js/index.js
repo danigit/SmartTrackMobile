93bdf3f388143c0f9a52edfb6c9dc4db6905c7b5
danigit <ds.acconto@gmail.com>
2018-10-04 23:31:33 +0200

    FILE SYSTEM INTERACTION
    Create le funzioni che leggono e creano/scrivono un file nel file system del dispositivo. Le funzioni hanno accesso anche al di fuori della sandbox dell'applicazione
    
    index.js -> writeToExternalRootDirectory(), readConfiguration() - la prima funzione crea un file nel file system e la seconda lo legge

--- www/js/index.js
+++ www/js/index.js
@@ -31,22 +31,86 @@ let app = {
     },
 };
 
+let storage = {
+    /**
+     * Funzione che crea un file nel file system del dispositivo
+     * @param fileToCreate - nome del file
+     * @param fileContent - il contenuto del file
+     */
+    writeToExternalRootDirectory: function(fileToCreate, fileContent){
+        let fileName = fileToCreate;
+        let data = fileContent;
+
+        window.resolveLocalFileSystemURL( cordova.file.externalRootDirectory, function( directoryEntry ) {
+            directoryEntry.getFile(fileName, { create: true }, function( fileEntry ) {
+                    fileEntry.createWriter( function( fileWriter ) {
+                            fileWriter.onwriteend = function( result ) {
+                                //TODO mostrare che il contenuto sul file e' stato scritto
+                            };
+                            fileWriter.onerror = function( error ) {
+                                //TODO mostrare che non e' possibile scrivere sul file
+                            };
+
+                            fileWriter.write( data );
+                    }, function( error ) {
+                            //TODO mostrare che c'e' stato un errore
+                        }
+                    );
+                },
+                function( error ) {
+                    //TODO  mostrare che c'e' stato un errore
+                }
+            );},
+            function( error ) {
+                //TODO mostrare che c'e' stato un errore
+            }
+        );
+    },
+
+    /**
+     * Funzione che legge un file dal file system del dispositivo
+     * @param fileToRead - nome del file da leggere
+     */
+    readConfiguration: function(fileToRead){
+        let fileName = fileToRead;
+
+        window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory, function (directoryEntry) {
+            directoryEntry.getFile(fileName, {create: false}, function (fileEntry) {
+                fileEntry.file(function (file) {
+                    let reader = new FileReader();
+
+                    reader.onloadend = function () {
+                        //TODO usare il contenuto letto dal file
+                    };
+
+                    reader.readAsText(file);
+                }, app.onErrorReadFile());
+            })
+        });
+    },
+
+    onErrorReadFile: function(){
+        //TODO mostrare messaggion di errore
+    },
+};
 
 let kits = {
-  getKits: function () {
-      $.ajax({
+
+    /**
+     * Funzione che recupera tutti i kit attivi
+     */
+    getKits: function () {
+        $.ajax({
           type: "GET",
           url: "http://danielfotografo.altervista.org/smartTrack/php/ajax/get_all_kits.php",
-      }).done(function (data) {
-          console.log(data);
-          alert('kit getted ');
-          alert(data);
-      }).fail(function (a, b, c) {
-          console.log('fail: ' + b + '|' + c );
-          alert('fail');
-      });
-  }
+        }).done(function (data) {
+            //TODO inserire i kit nella tabella
+        }).fail(function (a, b, c) {
+            //TODO mostrare messaggio di errore
+        });
+    }
 };
+
 
 $(document).ready( function () {
     app.initialize();
